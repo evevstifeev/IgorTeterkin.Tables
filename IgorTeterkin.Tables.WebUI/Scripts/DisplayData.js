9ad635b3fde7f2a8e3a1af8c64b4a7c1dba59e30
Evgenii_Evstifeev@epam.com <Evgenii_Evstifeev@epam.com>
2021-03-12 14:20:34 +0400

    Add error controller for page not found  - Add undefined check to DisplayData.js -  Add more descriptive comments - Fix font-family - Remove navbar

--- IgorTeterkin.Tables.WebUI/Scripts/DisplayData.js
+++ IgorTeterkin.Tables.WebUI/Scripts/DisplayData.js
@@ -2,17 +2,26 @@
     $.ajax({
         url: '/Data/GetData',
         type: 'GET',
+        // params has 3 properties: success - response status (true, false),
+        // generalData - data from .json file,
+        // currencyRatio - string from remote resource
         success: function (params) {
+            if (!params.success) {
+                location.href = "/Error/PageNotFound";
+            }
+            // Format for float numbers
             const floatFormatter = new Intl.NumberFormat('ru-RU', {
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 2,
             }),
+                // Format for dates
                 dateFormatOptions = {
                     day: "numeric",
                     month: "numeric",
                     year: "numeric"
                 };
 
+            // Read data from params, declare some auxiliary jQuery objects
             var data = JSON.parse(params.generalData),
                 policyGeneralData = data.generalInfo.policyGeneralData,
                 naturalPersonData = data.generalInfo.policyHolder.policyParticipant.naturalPersonData,
@@ -23,38 +32,48 @@
                 $naturalPersonData,
                 totalPremium = 0,
                 lastSumInsured,
-                currencyRatio = (Math.round(params.currencyRatio.replace(',', '.') * 100) / 100).toFixed(2);
+                currencyRatio = params.currencyRatio == "" ? "Не доступно"
+                    : floatFormatter.format((Math.round(params.currencyRatio.replace(',', '.') * 100) / 100).toFixed(2));
 
-            $contractInfo = $('<tr>');
-            $contractInfo.append('<td>' + policyGeneralData.documentBusinessNumber + '</td>');
-            $contractInfo.append('<td>' + new Date(policyGeneralData.startDate).toLocaleString("ru-RU", dateFormatOptions) + '</td>');
-            $contractInfo.append('<td>' + new Date(policyGeneralData.endDate).toLocaleString("ru-RU", dateFormatOptions) + '</td>');
-            $contractInfo.append('</tr>');
-            $('#contractInfo').append($contractInfo);
+            // Append data for tables
+            // Contact info:
+            if (typeof policyGeneralData !== 'undefined') {
+                $contractInfo = $('<tr>');
+                $contractInfo.append('<td>' + policyGeneralData.documentBusinessNumber + '</td>');
+                $contractInfo.append('<td>' + new Date(policyGeneralData.startDate).toLocaleString("ru-RU", dateFormatOptions) + '</td>');
+                $contractInfo.append('<td>' + new Date(policyGeneralData.endDate).toLocaleString("ru-RU", dateFormatOptions) + '</td>');
+                $contractInfo.append('</tr>');
+                $('#contractInfo').append($contractInfo);
+            }
 
-            $naturalPersonData = $('<tr>');
-            $naturalPersonData.append(`<td> ${naturalPersonData.lastName} ${naturalPersonData.firstName} ${naturalPersonData.fatherName}</td>`);
-            $naturalPersonData.append('<td>' + new Date(naturalPersonData.birthDate).toLocaleString("ru-RU", dateFormatOptions) + '</td>');
-            $naturalPersonData.append(`<td> ${naturalPersonData.genderCode[0]}</td>`);
-            $naturalPersonData.append('</tr>');
-            $('#naturalPersonData').append($naturalPersonData);
+            // Natural person data:
+            if (typeof policyGeneralData !== 'undefined') {
+                $naturalPersonData = $('<tr>');
+                $naturalPersonData.append(`<td> ${naturalPersonData.lastName} ${naturalPersonData.firstName} ${naturalPersonData.fatherName}</td>`);
+                $naturalPersonData.append('<td>' + new Date(naturalPersonData.birthDate).toLocaleString("ru-RU", dateFormatOptions) + '</td>');
+                $naturalPersonData.append(`<td> ${naturalPersonData.genderCode[0]}</td>`);
+                $naturalPersonData.append('</tr>');
+                $('#naturalPersonData').append($naturalPersonData);
+            }
 
-            $('#naturalPersonMainDocument tr:last')
-                .after(`<tr><td>Тип</td><td>${naturalPersonMainDocument.documentTypeCode}</td></tr>`);
-            $('#naturalPersonMainDocument tr:last')
-                .after(`<tr><td>Серия</td><td>${naturalPersonMainDocument.seria}</td></tr>`);
-            $('#naturalPersonMainDocument tr:last')
-                .after(`<tr><td>Номер</td><td>${naturalPersonMainDocument.number}</td></tr>`);
-            $('#naturalPersonMainDocument tr:last')
-                .after(`<tr><td>Код подразделения</td><td>${naturalPersonMainDocument.departmentCode}</td></tr>`);
-            $('#naturalPersonMainDocument tr:last')
-                .after(`<tr><td>Дата выдачи</td><td>${new Date(naturalPersonMainDocument.issueDate).toLocaleString("ru-RU", dateFormatOptions)
-                    }</td></tr>`);
-            $('#naturalPersonMainDocument tr:last')
-                .after(`<tr>'<td>Кем выдан</td><td>${naturalPersonMainDocument.issueParty}</td></tr>`);
-
-            if (risksDictionary.length > 0) {
-
+            // Natural person main document
+            if (typeof naturalPersonMainDocument !== 'undefined') {
+                $('#naturalPersonMainDocument tr:last')
+                    .after(`<tr><td>Тип</td><td>${naturalPersonMainDocument.documentTypeCode}</td></tr>`);
+                $('#naturalPersonMainDocument tr:last')
+                    .after(`<tr><td>Серия</td><td>${naturalPersonMainDocument.seria}</td></tr>`);
+                $('#naturalPersonMainDocument tr:last')
+                    .after(`<tr><td>Номер</td><td>${naturalPersonMainDocument.number}</td></tr>`);
+                $('#naturalPersonMainDocument tr:last')
+                    .after(`<tr><td>Код подразделения</td><td>${naturalPersonMainDocument.departmentCode}</td></tr>`);
+                $('#naturalPersonMainDocument tr:last')
+                    .after(`<tr><td>Дата выдачи</td><td>${new Date(naturalPersonMainDocument.issueDate).toLocaleString("ru-RU", dateFormatOptions)
+                        }</td></tr>`);
+                $('#naturalPersonMainDocument tr:last')
+                    .after(`<tr>'<td>Кем выдан</td><td>${naturalPersonMainDocument.issueParty}</td></tr>`);
+            }
+            // Risks
+            if (typeof risksDictionary !== 'undefined' && risksDictionary.length > 0) {
                 lastSumInsured = risks[risksDictionary.length - 1].sumInsured;
                 for (i = 0; i < risksDictionary.length; i++) {
                     $('#risksDictionary tr:last').after(`<tr>
@@ -67,16 +86,18 @@
                 }
             }
 
-            $('#significantInfo tr:last').after(`<tr>
+            // Valuable  information
+            $('#valuableInfo tr:last').after(`<tr>
         <td>${floatFormatter.format(lastSumInsured)}</td>
         <td>${floatFormatter.format(totalPremium)}</td>
-        <td>${floatFormatter.format(currencyRatio)}</td>
+        <td>${currencyRatio}</td>
     </tr>`);
         },
         error: function (error) {
-            // handle error here
-            alert('Error')
+            // If something happened redirects to the page (the previous page will not
+            // be accessible with browser back button
+            location.href = "/Error/PageNotFound";
         }
     });
-    
+
 }());
